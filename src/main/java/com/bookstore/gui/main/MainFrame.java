84eb451a7b068855677489a8f1d0c3536a960393
Le Ngoc Quy <nquyle2006@gmail.com>
2026-02-06 16:51:20 +0700

    add AppConstant, MoneyFormatter

--- src/main/java/com/bookstore/gui/main/MainFrame.java
+++ src/main/java/com/bookstore/gui/main/MainFrame.java
@@ -2,6 +2,7 @@ package com.bookstore.gui.main;
 
 import com.bookstore.dto.EmployeeDTO;
 import com.bookstore.gui.panel.SellingPanel;
+import com.bookstore.util.AppConstant;
 import com.formdev.flatlaf.FlatClientProperties;
 import com.formdev.flatlaf.extras.FlatSVGIcon;
 import javax.swing.*;
@@ -62,7 +63,7 @@ public class MainFrame extends JFrame {
         gbc.insets = new Insets(10, 0, 20, 0);
 
         JLabel lbWelcome1 = new JLabel("Chào Mừng Trở Lại Hệ Thống");
-        lbWelcome1.setFont(new Font("Segoe UI", Font.BOLD, 40));
+        lbWelcome1.setFont(new Font(AppConstant.FONT_NAME, Font.BOLD, 40));
         lbWelcome1.setForeground(Color.decode("#062D1E"));
         panel.add(lbWelcome1, gbc);
 
@@ -70,7 +71,7 @@ public class MainFrame extends JFrame {
 
         String username = employee.getEmployeeName();
         JLabel lbWelcome2 = new JLabel("Xin chào, " + username + "!");
-        lbWelcome2.setFont(new Font("Segoe UI", Font.PLAIN, 28));
+        lbWelcome2.setFont(new Font(AppConstant.FONT_NAME, Font.PLAIN, 28));
         lbWelcome2.setForeground(Color.GRAY);
         panel.add(lbWelcome2, gbc);
 
@@ -86,13 +87,13 @@ public class MainFrame extends JFrame {
 
         JLabel lbHeader1 = new JLabel("Quản Lý");
         lbHeader1.setForeground(Color.WHITE);
-        lbHeader1.setFont(new Font("Segoe UI", Font.BOLD, 30));
+        lbHeader1.setFont(new Font(AppConstant.FONT_NAME, Font.BOLD, 30));
         lbHeader1.setAlignmentX(Component.CENTER_ALIGNMENT);
         lbHeader1.setHorizontalAlignment(SwingConstants.CENTER);
 
         JLabel lbHeader2 = new JLabel("Bán Sách");
         lbHeader2.setForeground(Color.WHITE);
-        lbHeader2.setFont(new Font("Segoe UI", Font.BOLD, 30));
+        lbHeader2.setFont(new Font(AppConstant.FONT_NAME, Font.BOLD, 30));
         lbHeader2.setAlignmentX(Component.CENTER_ALIGNMENT);
         lbHeader2.setHorizontalAlignment(SwingConstants.CENTER);
 
@@ -117,7 +118,7 @@ public class MainFrame extends JFrame {
         btnAccount = createMenuButton("Tài Khoản", "account_icon.svg");
 
         btnLogout = new JButton("Đăng xuất");
-        btnLogout.setFont(new Font("Segoe UI", Font.BOLD, 16));
+        btnLogout.setFont(new Font(AppConstant.FONT_NAME, Font.BOLD, 16));
         btnLogout.setForeground(Color.WHITE);
         btnLogout.setBackground(Color.decode("#062D1E"));
         btnLogout.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
@@ -186,7 +187,7 @@ public class MainFrame extends JFrame {
 
     private JButton createMenuButton(String text, String iconName) {
         JButton btn = new JButton(text);
-        btn.setFont(new Font("Segoe UI", Font.BOLD, 16));
+        btn.setFont(new Font(AppConstant.FONT_NAME, Font.BOLD, 16));
         btn.setForeground(Color.WHITE);
         btn.setBackground(Color.decode("#062D1E"));
         btn.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
@@ -216,7 +217,7 @@ public class MainFrame extends JFrame {
         JPanel p = new JPanel(new GridBagLayout());
         p.setBackground(Color.WHITE);
         JLabel lb = new JLabel(text);
-        lb.setFont(new Font("Segoe UI", Font.BOLD, 40));
+        lb.setFont(new Font(AppConstant.FONT_NAME, Font.BOLD, 40));
         lb.setForeground(Color.LIGHT_GRAY);
         p.add(lb);
         return p;
